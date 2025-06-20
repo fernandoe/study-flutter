e7a5f84f96dc1bf97db3f54d5e602272e881e597
Fernando Espíndola <fernando.espindola@adp.com>
2025-06-19 23:44:15 -0300

    test

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -5,15 +5,35 @@ main() => runApp(PerguntaApp());
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
               title: Text('Perguntas'),
             ),
-            body: Text('Olá Flutter!'),
+            body: Column(
+              children: [
+                Text(perguntas[0]),
+                ElevatedButton(
+                  child: Text("Resposta 1"),
+                  onPressed: () {},
+                ),
+                ElevatedButton(
+                  child: Text("Resposta 2"),
+                  onPressed: () {},
+                ),
+                ElevatedButton(
+                  child: Text("Resposta 3"),
+                  onPressed: () {},
+                )
+              ],
+            )
         ),
     );
   }
